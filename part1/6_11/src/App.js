ee3d74f97eb889b2d79084f7c86a8d8694749a1f
Valentin Khomutenko <valentine.khomutenko@gmail.com>
2023-02-26 23:36:03 +0400

    add exercise 11

--- part1/6_11/src/App.js
+++ part1/6_11/src/App.js
@@ -8,7 +8,10 @@ const Button = ({onClick, text}) => {
 
 const StatisticLine = ({text, value}) => {
     return (
-        <p>{text} {value}</p>
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
     )
 }
 
@@ -24,12 +27,16 @@ const Statistics = ({good, neutral, bad}) => {
     }
     return (<>
         <h2>Statistics</h2>
-        <StatisticLine text="good" value={good}/>
-        <StatisticLine text="neutral" value={neutral}/>
-        <StatisticLine text="bad" value={bad}/>
-        <StatisticLine text="all" value={all}/>
-        <StatisticLine text="average" value={average}/>
-        <StatisticLine text="positive" value={positive}/>
+        <table>
+            <tbody>
+            <StatisticLine text="good" value={good}/>
+            <StatisticLine text="neutral" value={neutral}/>
+            <StatisticLine text="bad" value={bad}/>
+            <StatisticLine text="all" value={all}/>
+            <StatisticLine text="average" value={average}/>
+            <StatisticLine text="positive" value={positive}/>
+            </tbody>
+        </table>
     </>)
 }
 const App = () => {
